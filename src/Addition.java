e7192abadab71480f057081d7fd4681ec7e2bfe3
PL LAB <PL LAB@PLLAB69>
2023-08-10 11:37:57 +0530

    First Version

---
+++ src/Addition.java
@@ -0,0 +1,6 @@
+
+public class Addition {
+	public void add(int x, int y) {
+		
+	}
+}
---
+++ src/Mycalc.java
@@ -0,0 +1,8 @@
+
+public class Mycalc {
+	public static void main(String args[]) {
+		Addition a = new Addition();
+		Subtraction s = new Subtraction();
+
+}
+}
